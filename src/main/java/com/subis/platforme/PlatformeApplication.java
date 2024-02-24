ccaa670ef1c81028419fcae5c270d857d8d1b717
yuchaemin2 <kusuri94@gmail.com>
2024-02-24 11:18:37 +0900

    Feat: 초기 설정 세팅

---
+++ src/main/java/com/subis/platforme/PlatformeApplication.java
@@ -0,0 +1,13 @@
+package com.subis.platforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subis/platforme/PlatformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subis.platforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
